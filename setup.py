14c02e160b8d6461f8d34d3d092ac1f492989977
George Munyoro <george.guva@outlook.com>
2020-10-06 11:02:51 -0700

    Setup basic setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Skaak',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A Python Chess Library',
+    author='George Munyoro',
+    license='MIT',
+)
