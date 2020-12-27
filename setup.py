8e7749714196995cd9b463fd75b64fab29954a1b
George Munyoro <george.guva@outlook.com>
2020-12-27 15:36:50 +0200

    chore: update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="Skaak",
     packages=find_packages(include=["skaak"]),
-    version="0.1.0",
+    version="0.12.5",
     description="A Python Chess Library",
     author="George Munyoro",
     license="MIT",
