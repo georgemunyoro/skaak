895c5a66393d878394cb5f41ce5fa42f9e6f4031
George Munyoro <george.guva@outlook.com>
2020-12-27 13:13:27 +0200

    - feat: add method for converting x88 board index to algebraic notation 	- e.g: 113 -> B1 or 68 -> E4
     - chore: add repr to move class:
    	- moves now printed as "a3a4"
    
     - simplified invalid move exception

--- skaak/chess.py
+++ skaak/chess.py
@@ -10,6 +10,12 @@ NORTH, EAST, SOUTH, WEST = -16, 1, 16, -1
 PIECES = "rnbqkpRNBQKP"
 
 
+def convert_x88_board_ref_to_san(x88_board_ref: int) -> str:
+    pos_rank = 8 - (x88_board_ref // 16)
+    pos_file = "abcdefgh"[x88_board_ref % 16]
+    return f"{pos_file}{pos_rank}"
+
+
 class Move:
     def __init__(self, **kwargs):
         self.initial_square: int = kwargs.pop("initial_square", None)
@@ -19,6 +25,9 @@ class Move:
         self.capture: bool = kwargs.pop("capture", None)
         self.score: int = kwargs.pop("score", None)
         self.pseudo: bool = kwargs.pop("pseudo", None)
+
+    def __repr__(self) -> str:
+        return f"{convert_x88_board_ref_to_san(self.initial_square)}{convert_x88_board_ref_to_san(self.target_square)}"
 
 
 RANKS = "abcdefgh"
@@ -82,6 +91,4 @@ MOVES = {
 
 
 class InvalidMove(Exception):
-    def __init__(self, message, errors):
-        super().__init__(message)
-        self.errors = errors
+    pass
